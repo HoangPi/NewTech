08ba3f36e3e9acd666d883a47442ecfa580b1e9c
Phi Vo <voduchoangphi@gmail.com>
2023-11-06 10:57:38 +0700

    First init, delete unused files

--- client/src/components/AddBlog.js
+++
@@ -1,8 +0,0 @@
-export const newBlog = (props) => {
-    return (
-        <form>
-            <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example" />
-            <button class="btn btn-primary" type="submit" onClick={props.dosmth}>Post</button>
-        </form>
-    )
-}
---
+++ client/src/pages/landingPage.js
@@ -0,0 +1,9 @@
+import { NavigationBar } from "../components/NavBar"
+
+export const LandingPage = () =>{
+    return(
+        <div>
+            <NavigationBar></NavigationBar>
+        </div>
+    )
+}
